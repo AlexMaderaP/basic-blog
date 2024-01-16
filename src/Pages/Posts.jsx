835042f67c5c73ffa2b7123ae0bbaa852a8a4df1
AlexMaderaP <alejandro.maderap@gmail.com>
2024-01-16 12:57:11 -0600

    Add Posts route and page

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 function Posts() {
-  return <div>Posts</div>;
+  const posts = useLoaderData();
+
+  return (
+    <div className="container">
+      <h1 className="page-title">Posts</h1>
+      <div className="card-grid">
+        {posts.map((post) => (
+          <div key={post.id} className="card">
+            <div className="card-header">{post.title}</div>
+            <div className="card-body">
+              <div className="card-preview-text">{post.body}</div>
+            </div>
+            <div className="card-footer">
+              <Link className="btn" to="./">
+                View
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Posts;
--- src/router.jsx
+++ src/router.jsx
@@ -9,7 +9,13 @@ export const router = createBrowserRouter([
     element: <NavLayout />,
     children: [
       { path: "*", element: <Posts /> },
-      { path: "/posts", element: <Posts /> },
+      {
+        path: "/posts",
+        element: <Posts />,
+        loader: ({ request: { signal } }) => {
+          return fetch("http://127.0.0.1:3000/posts", { signal });
+        },
+      },
       { path: "/users", element: <Users /> },
       { path: "/todos", element: <Todos /> },
     ],
