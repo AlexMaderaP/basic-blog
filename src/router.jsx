23349929245b68e1d022217af194f01ca3c30a1b
AlexMaderaP <alejandro.maderap@gmail.com>
2024-01-16 14:46:02 -0600

    Add todos page and loader

--- src/router.jsx
+++ src/router.jsx
@@ -53,7 +53,13 @@ export const router = createBrowserRouter([
           },
         ],
       },
-      { path: "/todos", element: <Todos /> },
+      {
+        path: "/todos",
+        element: <Todos />,
+        loader: ({ request: { signal } }) => {
+          return fetch("http://127.0.0.1:3000/todos", { signal });
+        },
+      },
     ],
   },
 ]);
